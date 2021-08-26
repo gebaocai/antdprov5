bef897664cbffce455aec09328a16b11670ed674
gebaocai <gebaocai@gmail.com>
2021-08-26 19:58:15 +0800

    use umi request interceptor

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -1,13 +1,10 @@
 // @ts-ignore
 /* eslint-disable */
-import { request } from 'umi';
+import request from '@/utils/request';
 
 /** 获取当前的用户 GET /api/currentUser */
 export async function currentUser(options?: { [key: string]: any }) {
   return request<API.CurrentUser>('/api/currentUser', {
-    headers:{
-      'Authorization': `Bear ${localStorage.getItem('token')}`
-    },
     method: 'GET',
     ...(options || {}),
   });
