d542c67f4320aa7e573f14177a07d5d46b2846e3
gebaocai <gebaocai@gmail.com>
2021-09-14 09:05:04 +0800

    show modal when edit permission

--- src/pages/system/permission/Index.tsx
+++ src/pages/system/permission/Index.tsx
@@ -27,6 +27,7 @@ import { Table, Divider, Menu, Dropdown } from 'antd';
 import { ColumnsType } from 'antd/es/table';
 import { DownOutlined } from '@ant-design/icons';
 import { FormattedMessage } from 'umi';
+import { Drawer} from 'antd';
 
 
 // const data:TableListItem[] = [
@@ -36,97 +37,108 @@ import { FormattedMessage } from 'umi';
 //   key: "1"}
 // ];
 
-const columns: ColumnsType<TableListItem> = [
-  {
-    title: '菜单名称',
-    dataIndex: 'name',
-    key: 'name'
-  }, {
-    title: '菜单类型',
-    dataIndex: 'menuType',
-    key: 'menuType',
-    render: function(text) {
-      if (text == 0) {
-        return '菜单'
-      } else if (text == 1) {
-        return '菜单'
-      } else if (text == 2) {
-        return '按钮/权限'
-      } else {
-        return text
-      }
-    }
-  },/*{
-    title: '权限编码',
-    dataIndex: 'perms',
-    key: 'permissionCode',
-  },*/{
-    title: 'icon',
-    dataIndex: 'icon',
-    key: 'icon'
-  },
-  {
-    title: '组件',
-    dataIndex: 'component',
-    key: 'component',
-    ellipsis: true,
-  },
-  {
-    title: '路径',
-    dataIndex: 'url',
-    key: 'url',
-    ellipsis: true,
-  },
-  {
-    title: '排序',
-    dataIndex: 'sortNo',
-    key: 'sortNo',
-    width: 70
-  },
-  {
-    title: '操作',
-    dataIndex: 'action',
-    fixed: true,
-    render: () => (
-      <Space size="middle">
-        <a>编辑</a>
-        <Divider type="vertical" />
-        <Dropdown overlay={menu}>
-          <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-          更多 <DownOutlined />
-          </a>
-        </Dropdown>
-      </Space>
-    ),
-    // align: 'center',
-    // width: 150
-  }
-];
 
-const menu = (
-  <Menu>
-    <Menu.Item key="0">
-      {/* <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com"> */}
-        详情
-      {/* </a> */}
-    </Menu.Item>
-    <Menu.Item key="1">
-      {/* <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com"> */}
-        添加下级
-      {/* </a> */}
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="3" disabled>
-      删除
-    </Menu.Item>
-  </Menu>
-);
 
 const TableList: React.FC = () => {
 
   const {loading, data} = useRequest(permissionTree);
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const columns: ColumnsType<TableListItem> = [
+    {
+      title: '菜单名称',
+      dataIndex: 'name',
+      key: 'name'
+    }, {
+      title: '菜单类型',
+      dataIndex: 'menuType',
+      key: 'menuType',
+      render: function(text) {
+        if (text == 0) {
+          return '菜单'
+        } else if (text == 1) {
+          return '菜单'
+        } else if (text == 2) {
+          return '按钮/权限'
+        } else {
+          return text
+        }
+      }
+    },/*{
+      title: '权限编码',
+      dataIndex: 'perms',
+      key: 'permissionCode',
+    },*/{
+      title: 'icon',
+      dataIndex: 'icon',
+      key: 'icon'
+    },
+    {
+      title: '组件',
+      dataIndex: 'component',
+      key: 'component',
+      ellipsis: true,
+    },
+    {
+      title: '路径',
+      dataIndex: 'url',
+      key: 'url',
+      ellipsis: true,
+    },
+    {
+      title: '排序',
+      dataIndex: 'sortNo',
+      key: 'sortNo',
+      width: 70
+    },
+    {
+      title: '操作',
+      dataIndex: 'action',
+      fixed: true,
+      render: () => (
+        <Space size="middle">
+          <Button onClick={showDrawer}>编辑</Button>
+          <Divider type="vertical" />
+          <Dropdown overlay={menu}>
+            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+            更多 <DownOutlined />
+            </a>
+          </Dropdown>
+        </Space>
+      ),
+      align: 'center',
+      width: 200
+    }
+  ];
+  
+  const menu = (
+    <Menu>
+      <Menu.Item key="0">
+        {/* <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com"> */}
+          
+          <Button onClick={showDrawer}>详情</Button>
+        {/* </a> */}
+      </Menu.Item>
+      <Menu.Item key="1">
+        {/* <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com"> */}
+          添加下级
+        {/* </a> */}
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="3" disabled>
+        删除
+      </Menu.Item>
+    </Menu>
+  );
 
   return (
+    <>
      <Card>
        <Button>点我！</Button>
        <Table<TableListItem> 
@@ -134,6 +146,12 @@ const TableList: React.FC = () => {
         dataSource={data} 
         pagination={false} />
      </Card>
+     <Drawer title="Basic Drawer" placement="right" onClose={onClose} visible={visible}>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </Drawer>
+    </>
       
   );
 };
